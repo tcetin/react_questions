8a6e50c9209f4f5ce390890c114518e83f01b686
tcetin <tcetin@users.noreply.github.com>
2020-07-08 23:20:13 +0300

    Update answers.js

--- answers.js
+++ answers.js
@@ -77,3 +77,42 @@ const App = (props) => <FocusableInput focused={props.focused} />;
 document.body.innerHTML = "<div id='root'></div>";
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
+    
+//3. Change Username
+    
+class Username extends React.Component {
+  state = { value: "" };
+
+  changeValue(value) {
+    this.setState({ value });
+  }
+
+  render() {
+    const { value } = this.state;
+    return <h1>{value}</h1>;
+  }
+}
+
+function App() {
+  const ref = React.useRef();
+  const inputRef = React.useRef();
+  function clickHandler() {
+    ref.current.changeValue(inputRef.current.value);
+  }
+
+  return (
+    <div>
+      <button onClick={clickHandler}>Change Username</button>
+      <input type="text" ref={inputRef} />
+      <Username ref={ref} />
+    </div>
+  );
+}
+
+document.body.innerHTML = "<div id='root'></div>";
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+document.querySelector("input").value = "John Doe";
+document.querySelector("button").click();
+setTimeout(() => console.log(document.getElementById("root").innerHTML));
