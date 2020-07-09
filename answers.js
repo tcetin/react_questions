f1e091e77a96b35bfaba540cde92ab3e7b4be280
tcetin <tcetin@users.noreply.github.com>
2020-07-09 12:02:11 +0300

    Update answers.js

--- answers.js
+++ answers.js
@@ -211,3 +211,42 @@ ReactDOM.render(<ImageGallery links={ links } />,
                 rootElement);
 document.querySelectorAll('.remove')[0].click();
 console.log(rootElement.innerHTML);
+
+//6. Todo List
+
+const TodoItem = (props) => <li onClick={props.onClick}>{props.item.text}</li>
+
+class TodoList extends React.Component {
+  render() {
+    const { items, onListClick } = this.props;
+    return (<ul onClick={onListClick}>
+      {items.map((item, index) => 
+                 <TodoItem item={item} key={index} onClick={this.handleItemClick.bind(this, item)}/>)}
+    </ul>);
+  }
+  
+  handleItemClick(item, event) {
+    // Write your code here
+      if(item.done){
+      event.stopPropagation();
+      return;
+    }
+    this.props.onItemClick(item,event);
+  }
+}
+
+
+const items = [ { text: 'Buy grocery', done: true },
+  { text: 'Play guitar', done: false },
+  { text: 'Romantic dinner', done: false }
+];
+
+const App = (props) => <TodoList
+  items={props.items}
+  onListClick={(event) => console.log("List clicked!")}
+  onItemClick={(item, event) => { console.log(item, event) }}
+/>;
+
+document.body.innerHTML = "<div id='root'></div>";
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App items={items}/>, rootElement);
